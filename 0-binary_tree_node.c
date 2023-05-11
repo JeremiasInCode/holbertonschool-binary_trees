646ee193bc4881a50331534765f988896f96fe2d
jeremias <jeremiaserba7894@gmail.com>
2023-05-11 15:53:06 -0700

    Test

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,15 +3,20 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *newNode = NULL;
-
+    
     newNode = malloc(sizeof(binary_tree_t));
     if (!newNode)
         return (NULL);
     
-    ((parent) ? newNode->parent = parent : NULL);
+    newNode->parent = NULL;
+
+    if (parent)
+    {
+        newNode->parent = parent;
+    }
+
     newNode->right = NULL;
     newNode->left = NULL;
     newNode->n = value;
-
     return (newNode);
 }
