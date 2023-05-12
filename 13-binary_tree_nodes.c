093e9429afd479d5a07a8b6f14c2502b8f6dc085
jeremias <jeremiaserba7894@gmail.com>
2023-05-12 12:35:51 -0700

    Task 13 - First commit

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - Write a function that counts the nodes at least 1 child.
+ * @tree: tree
+ * Return: count of nodes with this feature.
+ */
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t counter = 0;
+
+    if (!tree)
+        return (0);
+
+    if (tree->left || tree->right)
+        counter++;
+
+    counter += binary_tree_nodes(tree->left);
+    counter += binary_tree_nodes(tree->right);
+    return (counter);
+}
