1fd88956057e398588f264074c1f0221d189068e
jeremias <jeremiaserba7894@gmail.com>
2023-05-11 17:14:22 -0700

    Task 1 - Second commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -15,6 +15,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     binary_tree_t *newNode = NULL;
 
     newNode = binary_tree_node(parent, value);
+    if (!parent)
+        return (NULL);
 
     if (parent->left == NULL)
     {
