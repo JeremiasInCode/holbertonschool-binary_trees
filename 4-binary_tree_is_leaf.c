5f5cf099da55885bb51c84a3f335882e2114d343
jeremias <jeremiaserba7894@gmail.com>
2023-05-12 14:24:51 -0700

    Fix betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_leaf - function that checks if node is a leaf
+ * binary_tree_is_leaf - function that checks if node is a leaf.
  * @node: node
  * Return: 1 is a leaf or 0
- */
+*/
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (!node)
 		return (0);
-	
+
 	return (node->left == NULL && node->right == NULL);
 }
