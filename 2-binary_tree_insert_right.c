27d78c6dc7a99e6d5d8dd804e68410aa46b2b8b1
jeremias <jeremiaserba7894@gmail.com>
2023-05-11 17:38:04 -0700

    Task 2 - First commit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @value: Value inside the node.
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode = NULL;
+
+    if (!parent)
+        return (NULL);
+    
+    newNode = malloc(sizeof(binary_tree_t));
+    if (!newNode)
+        return (NULL);
+
+    if (parent->right == NULL)
+    {
+        parent->right = newNode;
+    }
+    else if (parent->right != NULL)
+    {
+        newNode->right = parent->right;
+        parent->right = newNode;
+        newNode->right->parent = newNode;
+    }
+
+}
