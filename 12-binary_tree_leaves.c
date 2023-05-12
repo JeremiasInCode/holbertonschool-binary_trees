ad6144df562a8811ed98f21c2c5accbb87bcca1b
Macj04 <miguelcoa49@gmail.com>
2023-05-12 11:55:51 -0700

    task 12, similar of task 11

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary t
+ * @tree: tree
+ * Return: leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+        size_t leaves = 0;
+
+        if (tree == NULL)
+                return (0);
+        if (tree->left == NULL && tree->right == NULL)
+                return (1);
+        if (tree->left)
+                leaves += binary_tree_leaves(tree->left);
+        if (tree->right)
+                leaves += binary_tree_leaves(tree->right);
+        return (leaves);
+}
