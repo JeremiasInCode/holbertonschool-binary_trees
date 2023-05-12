ce062c27e9e5ce1a3ce19b3b413bba9a4b0452c0
jeremias <jeremiaserba7894@gmail.com>
2023-05-12 07:25:39 -0700

    Task 7 - First commit

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @value: Value inside the node.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree || !func)
+        return;
+
+    binary_tree_inorder(tree->left, func);
+    binary_tree_inorder(tree->right, func);
+    func(tree->n);
+}
