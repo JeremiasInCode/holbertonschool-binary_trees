2a229287dc0b5a144323e18f4c6925cf2556bb26
jeremias <jeremiaserba7894@gmail.com>
2023-05-11 17:00:26 -0700

    Task 1 - First commit

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * struct binary_tree_s - Binary tree node
+ *
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ * @value: Value inside the node.
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *newNode = NULL;
+
+    newNode = binary_tree_node(parent, value);
+
+    if (parent->left == NULL)
+    {
+        parent->left = newNode;
+    }
+    else if (parent->left != NULL)
+    {
+        newNode->left = parent->left;
+        parent->left = newNode;
+    }
+    return (newNode);
+}
