3e68e4a41f7ad587203c08d0da1243aa2034733a
jeremias <jeremiaserba7894@gmail.com>
2023-05-12 12:47:07 -0700

    Task 14 - Three commit

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,11 +23,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    size_t left_h = 1, right_h = 1;
+
 	if (tree == NULL)
 		return (0);
 
-	int left_h = binary_tree_height(tree->left);
-	int right_h = binary_tree_height(tree->right);
+	left_h = binary_tree_height(tree->left);
+	right_h = binary_tree_height(tree->right);
 
 	return (left_h - right_h);
 }
