161e74d02e750e876e1f940686da211a36ed4437
jeremias <jeremiaserba7894@gmail.com>
2023-05-11 18:13:27 -0700

    Task 3 -  Second commit

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -14,12 +14,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
     if (!tree)
         return;
-        
-    if (tree->left)
-        binary_tree_delete(tree->left);
-    else if (tree->right)
-        binary_tree_delete(tree->right);
-
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
     free(tree);
-    
 }
