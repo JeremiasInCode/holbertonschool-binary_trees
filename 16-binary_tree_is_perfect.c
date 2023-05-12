8fd16020d9081a6ca63d5bf2fba5a53471c92e8a
jeremias <jeremiaserba7894@gmail.com>
2023-05-12 13:45:43 -0700

    Task 16 - Fourth commit

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,7 +21,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_h = 1, right_h = 1;
 
 	if (tree == NULL)
-		return (0);
+		return (1);
     
     left_h += binary_tree_height(tree->left);
     right_h += binary_tree_height(tree->right);
