9b6a25d2bb2113802ec2489ab7690b15e9b7a2e0
Jeremias Erba <jeremiaserba7894@gmail.com>
2023-11-02 18:17:42 -0300

    Task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Binary tree node
+ * @parent: Pointer to the parent node
+ * @value: Value inside the node.
+ * Return: Pointer node
+*/
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = NULL;
+	newNode = binary_tree_node(parent, value);
+	if (parent->left == NULL)
+	{
+		parent->left = newNode;
+	}
+	else if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		parent->left = newNode;
+	}
+	return (newNode);
+}
